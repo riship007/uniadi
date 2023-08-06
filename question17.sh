617770532980e1d38251e132b4ba5df3a37c5a4e
rishi <riship007.com>
2023-08-06 13:56:16 +0930

    17

--- question17.sh
+++ question17.sh
@@ -1,18 +1,12 @@
-cd git-practice-03
 git checkout -b branch1
 git rm -r dir1/dir2
-git rm dir3/bar_copy
-echo "text" > newfile1
-git add newfile1
-echo "text" > README.md
-git add README.md
-git commit -m "massage"
-git checkout -b branch2
-echo "text" > dir1/dir2/foo_modified
-git add dir1/dir2/foo_modified
-echo "text" > dir1/dir3/newfile2
-git add dir1/dir3/newfile2
-echo "text" > LICENSE
-git add LICENSE
-git commit -m "massage"
+git mv dir1/dir2/foo dir1/foo
+echo "This is a new file added in branch1." > newfile1
+git add newfile1"
+git commit -m "Modify files to match the configuration for branch1"
 git checkout main
+git checkout -b branch2
+echo "This is a modified version of foo in branch2." > dir1/dir2/foo_modified
+echo "This is a new file added in branch2." > dir1/dir3/newfile2
+git add dir1/dir2/foo_modified dir1/dir3/newfile2
+git commit -m "Modify files to match the configuration for branch2"
